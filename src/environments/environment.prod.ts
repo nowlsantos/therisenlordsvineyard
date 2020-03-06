2099ae831c3103dc9b608e74d7a663b769da788f
Noel Santos <nowlsantos@gmail.com>
2020-03-07 00:01:58 +0800

    Added Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig: {
+        apiKey: 'AIzaSyD5DqaHUO54ZmUh2UoUad-TFi7VhY4rzqo',
+        authDomain: 'trlv-ccc.firebaseapp.com',
+        databaseURL: 'https://trlv-ccc.firebaseio.com',
+        projectId: 'trlv-ccc',
+        storageBucket: 'trlv-ccc.appspot.com',
+        messagingSenderId: '265286703593',
+        appId: '1:265286703593:web:d0bd2024189197b9285378'
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyD5DqaHUO54ZmUh2UoUad-TFi7VhY4rzqo',
+        authDomain: 'trlv-ccc.firebaseapp.com',
+        databaseURL: 'https://trlv-ccc.firebaseio.com',
+        projectId: 'trlv-ccc',
+        storageBucket: 'trlv-ccc.appspot.com',
+        messagingSenderId: '265286703593',
+        appId: '1:265286703593:web:d0bd2024189197b9285378'
+    }
 };
 
 /*
